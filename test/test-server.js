8773b44c5052d3399b6b279df913fe276b355277
sravankumar-n <35955190+sravankumar-n@users.noreply.github.com>
2021-06-15 19:39:19 +0530

    Update test-server.js

--- test/test-server.js
+++ test/test-server.js
@@ -27,7 +27,7 @@ describe('GET /', function () {
         if (err) {
           return done(err);
         }
-        chai.assert.isTrue(res.text.includes("<h1>Welcome to sample events application</h1>"));
+        chai.assert.isTrue(res.text.includes("<h1>Welcome to sample application</h1>"));
         return done();
       });
 
